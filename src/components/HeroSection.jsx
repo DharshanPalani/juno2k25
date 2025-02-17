57e6bb9529c72bac83a7897ed9a0f9a9bfa1d1e8
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-02-17 14:05:23 +0530

    Added links for rule book, day 1 and 2 form

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -83,17 +83,17 @@ function HeroSection() {
           </div>
           <div className="mt-8 flex flex-col md:flex-col items-center space-y-0 md:space-y-0 md:space-x-8">
             <br />
-            <button className="bg-yellow-500 text-black px-8 py-4 rounded-lg text-lg text-white px-16 py-4 rounded-lg text-lg font-bold md:w-auto hover:scale-105 hover:shadow-lg transition-all" onClick={handleRegisterClick}>
+            <a className="bg-yellow-500 text-black px-8 py-4 rounded-lg text-lg text-white px-16 py-4 rounded-lg text-lg font-bold md:w-auto hover:scale-105 hover:shadow-lg transition-all" href="https://drive.google.com/file/d/1fhKMbjfgYWQ5Gc1eCiO0Qr1jSlnIAvjh/view" target="_blank">
               Rule book
-            </button>
+            </a>
             <br />
-            <button className="bg-yellow-500 text-black px-8 py-4 rounded-lg text-lg text-white px-16 py-4 rounded-lg text-lg font-bold w-full md:w-auto hover:scale-105 hover:shadow-lg transition-all" onClick={handleRegisterClick}>
+            <a className="bg-yellow-500 text-black px-8 py-4 rounded-lg text-lg text-white px-16 py-4 rounded-lg text-lg font-bold w-full md:w-auto hover:scale-105 hover:shadow-lg transition-all" href="https://docs.google.com/forms/d/e/1FAIpQLScX6bKYLR2zxUwtMQqFlWgquP6WWxKXmhUOMfX3s_wfVEFEiw/viewform" target="_blank">
               Day-1
-            </button>
+            </a>
             <br />
-            <button className="bg-yellow-500 text-black px-8 py-4 rounded-lg text-lg text-white px-16 py-4 rounded-lg text-lg font-bold w-full md:w-auto hover:scale-105 hover:shadow-lg transition-all" onClick={handleRegisterClick}>
+            <a className="bg-yellow-500 text-black px-8 py-4 rounded-lg text-lg text-white px-16 py-4 rounded-lg text-lg font-bold w-full md:w-auto hover:scale-105 hover:shadow-lg transition-all" href="https://docs.google.com/forms/d/e/1FAIpQLSfUoaxuqk1wO4Ve5tPTvdlhQwexs2g1jhIaQzKtmdKYjfx1IA/viewform" target="_blank">
               Day-2
-            </button>
+            </a>
           </div>
         </div>
       </div>
